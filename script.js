2636d8770787c9290d5a2ee786db0172fbdd119f
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-11 10:07:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function mincost(arr)
 { 
+	let sum=0;
+	let len=0;
+	
+	let ans=arr.sort();
+	for(let i=0;i<arr.length;i++){
+		sum=sum[i]+sum[i+1];
+		 len+=sum;
+		i+=1;
+	}
+	return len;
 //write your code here
 // return the min cost
   
